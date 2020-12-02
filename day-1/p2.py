81f1e451c8f65d087c6bbcd9c5f1f21abfbbb04a
Alex Annan <lxnn.git@gmail.com>
2020-12-02 13:01:35 +0000

    Optimised p2 of day 1.

--- day-1/p2.py
+++ day-1/p2.py
@@ -3,10 +3,15 @@
 with open('input.txt') as file:
     nums = list(map(int, file))
 
+nums.sort()
+minval, maxval = min(nums), max(nums)
+
 target = 2020
 
 seen = set()
 for i, x in enumerate(nums):
+    if x + 2 * minval > target:
+        break
     for j, y in enumerate(nums[i+1:], i+1):
         complement = target - (x + y)
         if complement in seen:
