182665b66f6094a0331556997724f93b5d3063f0
Alex Annan <lxnn.git@gmail.com>
2020-12-04 17:27:19 +0000

    Make problem 2 of day 4 less verbose.

--- day-4/p2.py
+++ day-4/p2.py
@@ -2,63 +2,20 @@ import sys, re
 from typing import NamedTuple, Callable
 
 
-class FieldSpec(NamedTuple):
-    required:   bool
-    validator:  Callable[[str], bool]
-
-
-FIELDS = dict(
-    byr = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                            (m := re.fullmatch(r"[0-9]{4}", string))
-                        and (1920 <= int(m.group(0)) <= 2002)
-                    ),
-    ),
-    iyr = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                            (m := re.fullmatch(r"[0-9]{4}", string))
-                        and (2010 <= int(m.group(0)) <= 2020)
-                    ),
-    ),
-    eyr = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                            (m := re.fullmatch(r"[0-9]{4}", string))
-                        and (2020 <= int(m.group(0)) <= 2030)
-                    ),
-    ),
-    hgt = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                            (m := re.fullmatch(r"([0-9]+)(cm|in)", string))
-                        and int(m.group(1)) in {
-                                'cm': range(150, 194),
-                                'in': range(59, 77),
-                            }[m.group(2)]
-                    ),
-    ),
-    hcl = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                        re.fullmatch(r"#[0-9a-f]{6}", string)
-                    ),
-    ),
-    ecl = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(
-                        re.fullmatch(r"amb|blu|brn|gry|grn|hzl|oth", string)
-                    ),
-    ),
-    pid = FieldSpec(
-        required    = True,
-        validator   = lambda string: bool(re.fullmatch(r"[0-9]{9}", string)),
-    ),
-    cid = FieldSpec(
-        required    = False,
-        validator   = lambda string: True,
-    ),
+REQUIRED_FIELDS = dict(
+    byr = lambda s: 1920 <= int(s) <= 2002,
+    iyr = lambda s: 2010 <= int(s) <= 2020,
+    eyr = lambda s: 2020 <= int(s) <= 2030,
+    hgt = lambda s: bool(
+            (m := re.fullmatch(r"([0-9]+)(cm|in)", s))
+            and int(m.group(1)) in {
+                'cm': range(150, 194),
+                'in': range(59, 77),
+            }[m.group(2)]
+        ),
+    hcl = lambda s: re.fullmatch(r"#[0-9a-f]{6}", s),
+    ecl = lambda s: s in "amb blu brn gry grn hzl oth".split(),
+    pid = lambda s: re.fullmatch(r"[0-9]{9}", s),
 )
 
 
@@ -78,14 +35,12 @@ def entries(line_iterable):
 
 
 def isvalid(entry):
-    for fieldname, spec in FIELDS.items():
-        if fieldname not in entry:
-            if spec.required:
-                print(f"Missing field: {fieldname}")
-                return False
-            continue
-        if not spec.validator(entry[fieldname]):
-            print(f"Invalid value {entry[fieldname]} for field {fieldname}.")
+    for field, validator in REQUIRED_FIELDS.items():
+        if field not in entry:
+            print(f"Missing field: {field}")
+            return False
+        if not validator(entry[field]):
+            print(f"Invalid value {entry[field]} for field {field}.")
             return False
     return True
 
