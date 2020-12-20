b613479191a20ecf7f9568162985cd56a980ad4b
Alex Annan <lxnn.git@gmail.com>
2020-12-20 16:45:39 +0000

    Add alternative solutions for day 18

---
+++ day-18/p1-alt.py
@@ -0,0 +1,18 @@
+import sys, collections, re
+
+if len(sys.argv) == 2:
+    with open(sys.argv[1]) as file:
+        raw = file.read()
+else:
+    raw = sys.stdin.read()
+
+class Int(int):
+    def __add__(self, other):   return Int(int(self) + int(other))
+    def __sub__(self, other):   return Int(int(self) * int(other))
+    __radd__ = __add__
+    __rsub__ = __sub__
+
+print(sum(
+    eval(re.sub(r"(\d+)", r"Int(\1)", re.sub(r"\*", r"-", expression)))
+    for expression in raw.splitlines()
+))
