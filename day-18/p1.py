d6182f63ef3b2736d40f507fd5d65942073c15cb
Alex Annan <lxnn.git@gmail.com>
2020-12-22 10:44:28 +0000

    Add original solutions to day 18

---
+++ day-18/p1.py
@@ -0,0 +1,51 @@
+from utils import *
+
+if len(sys.argv) >= 2:
+    with open(sys.argv[1]) as file:
+        rawinput = file.read()
+else:
+    rawinput = sys.stdin.read()
+
+expressions = map(str.strip, rawinput.splitlines())
+
+def torpn(exp):
+    stack = []
+    for char in exp:
+        if char in digits:
+            yield int(char)
+            while stack and stack[-1] != '(':
+                yield stack.pop()
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                yield stack.pop()
+            assert stack and stack.pop() == '('
+            while stack and stack[-1] != '(':
+                yield stack.pop()
+        elif char == '+':
+            stack.append(char)
+        elif char == '*':
+            stack.append(char)
+        else:
+            continue
+    assert '(' not in stack and ')' not in stack
+    while stack:
+        yield stack.pop()
+
+def evalrpn(rpn):
+    stack = []
+    for item in rpn:
+        if isinstance(item, int):
+            stack.append(item)
+        elif item == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif item == '*':
+            stack.append(stack.pop() * stack.pop())
+    assert len(stack) == 1
+    return stack.pop()
+
+def evalexp(exp):
+    return evalrpn(torpn(exp))
+
+print(sum(map(evalexp, expressions)))
---
+++ day-18/p2.py
@@ -0,0 +1,48 @@
+from utils import *
+
+if len(sys.argv) >= 2:
+    with open(sys.argv[1]) as file:
+        rawinput = file.read()
+else:
+    rawinput = sys.stdin.read()
+
+expressions = map(str.strip, rawinput.splitlines())
+
+def torpn(exp):
+    stack = []
+    for char in exp:
+        if char in digits:
+            yield int(char)
+        elif char == '+':
+            stack.append(char)
+        elif char == '*':
+            while stack and stack[-1] != '(' and stack[-1] == '+':
+                yield stack.pop()
+            stack.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                yield stack.pop()
+            assert stack.pop() == '('
+        else:
+            continue
+    assert not containsany(stack, '()'), stack
+    yield from reversed(stack)
+
+def evalrpn(rpn):
+    stack = []
+    for item in rpn:
+        if isinstance(item, int):
+            stack.append(item)
+        elif item == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif item == '*':
+            stack.append(stack.pop() * stack.pop())
+    assert len(stack) == 1
+    return stack.pop()
+
+def evalexp(exp):
+    return evalrpn(torpn(exp))
+
+print(sum(map(evalexp, expressions)))
